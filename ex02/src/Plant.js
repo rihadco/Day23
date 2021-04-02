377387526a7730a821d5b9db99e8a412a39aae93
rihadco <rihad.covcic@gmail.com>
2021-04-02 09:24:56 +0200

    Vjezba za dan 23

---
+++ ex02/src/Plant.js
@@ -0,0 +1,15 @@
+// src/Plant.js
+
+import React, { Component } from "react";
+
+class Plant extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { type: "Flowering plant" };
+    }
+    render() {
+        return <h2>Plant type is {this.state.type}</h2>;
+    }
+}
+
+export default Plant;
